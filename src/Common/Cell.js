e8a08a4b5a94ff174468de9d7c3ca1cdb8128a2b
Alex Mejias <alexdmejias@gmail.com>
2016-09-17 12:33:54 -0400

    finished translating the Cell class

--- src/Common/Cell.js
+++ src/Common/Cell.js
@@ -3,12 +3,15 @@ class Cell {
     this.row = row;
     this.column = column;
 
-    this.links = {
+    this.links = new Map();
+
+    this.neighbors = {
       north: undefined,
       south: undefined,
       east: undefined,
       west: undefined
     };
+    
   }
 
   get row() {
@@ -19,40 +22,48 @@ class Cell {
     return this.column;
   }
 
-  setDirection(direction, value) {
-    this.directions[direction] = value;
+  // instead of creating a setter for each direction
+  setNeighbors(direction, value) {
+    this.neighbors[direction] = value;
   }
 
-  getDirection(direction) {
-    return this.directions[direction];
+  // instead of creating a getter for each direction
+  getNeighbors(direction) {
+    return this.neighbors[direction];
   }
 
-  link(cell, bidi = true) {
-    this.links[cell] = true;
-    if (bidi) {
-      cell.link(this, bidi);
+  link(cell, bidirectional = true) {
+    this.links.set(cell, true); 
+
+    if (bidirectional) {
+      cell.link(this, false);
     }
     return this;
   }
 
-  unlink(cell, bidi = true) {
-    delete this.links[cell];
-    if (bidi) {
-      cell.unlink(this, bidi);
+  unlink(cell, bidirectional = true) {
+    this.links.delete(cell);
+
+    if (bidirectional) {
+      cell.unlink(this, false);
     }
     return this;
   }
 
   links() {
-    return Object.keys(this.links);
+    return this.links.keys();
   }
 
   linked(cell) {
-    return Object.keys(this.links).indexOf(cell) > -1;
+    return this.links.has(cell) || false;
   }
 
   neighbors() {
-    // TODO
+    const list = [];
+    for(let direction in this.neighbors) {
+      list.push(direction);
+    }
+    return list;
   }
 }
 
