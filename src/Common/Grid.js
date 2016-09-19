94e34ffab7820d6dd095b55b8841b1a2ec0fe2e4
Alex Mejias <alexdmejias@gmail.com>
2016-09-18 22:58:17 -0400

    almost done with grid class

--- src/Common/Grid.js
+++ src/Common/Grid.js
@@ -5,7 +5,10 @@ class Grid {
     this.rows = rows;
     this.columns = columns;
 
-    configure_cells();
+    // this.grid;
+
+    this.grid = this.prepareGrid();
+    this.configureCells();
   }
 
   get rows() {
@@ -16,8 +19,61 @@ class Grid {
     return this.columns;
   }
 
-  configure_cells() {
+  prepareGrid() {
+    const grid = []
+    for (var i = 0; i < this.rows; i++) {
+      grid.push([]);
+      for (var h = 0; h < this.columns; h++) {
+        grid[i].push(new Cell(i, h));
+      }
+    }
 
+    return grid;
+  }
+
+  configureCells() {
+    this.eachCell().forEach((cell) => {
+      const {row, column} = cell;
+
+      // assign the neighbors for each cell
+      cell.north = this.getCell(row - 1, col);
+      cell.south = this.getCell(row + 1, col);
+      cell.west = this.getCell(row, col - 1);
+      cell.east = this.getCell(row, col + 1);
+    });
+  }
+
+  getCell(row, column) {
+    if ((row < 0) || (row > this.rows.length)) {
+      return false;
+    }
+
+    if ((column < 0) || (column > this.columns.length)) {
+      return false;
+    }
+
+    return this.grid[row][column];
+  }
+
+  randomCell() {
+    const row = Math.floor(Math.random() * this.rows);
+    const column = Math.floor(Math.random() * this.columns);
+
+    return this.grid[row][column];
+  }
+
+  size() {
+    return this.rows * this.columns;
+  }
+
+  eachRow() {
+    this.grid.forEach((row) => {
+      // rb: yield row
+    });
+  }
+
+  eachCell() {
+    // todo
   }
 }
 
