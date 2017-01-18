b17f39be2e8359b9ce41dd1502e29275e38c2ce2
Alex Mejias <alexdmejias@gmail.com>
2017-01-17 19:00:31 -0500

    adding socket  middleware

---
+++ src/middleware/socketio-middleware.js
@@ -0,0 +1,38 @@
+import io from 'socket.io-client';
+
+const socketMiddleware = (socket) => {
+  function _socketConnect () {
+    console.log('socketio connection established');
+
+    socket = io.connect('http://localhost:3005');
+    _registerCallbacks();
+  }
+
+  function _registerCallbacks () {
+    for (let event in eventsToListenTo) {
+      if (eventsToListenTo.hasOwnProperty(event)) {
+        socket.on(event, eventsToListenTo[event]);
+      }
+    }
+  }
+
+  const eventsToListenTo = {
+    'connection': () => { console.log('another player connected'); }
+  };
+
+  return (next) => (action) => {
+    const result = next(action);
+
+    if (action.type === 'SOCKET_CONNECT') {
+      _socketConnect();
+    } else if (action.socketEvent && socket && socket.emit) {
+      const payload = action.socketPayload || {};
+      console.log('%cTX: ' + action.socketEvent, 'color: red; font-weight: bold', payload);
+      socket.emit(action.socketEvent, payload);
+    }
+
+    return result;
+  };
+};
+
+export default socketMiddleware;
--- src/store.js
+++ src/store.js
@@ -1,23 +1,16 @@
-import {createStore, combineReducers, compose} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+
 import reducers from './reducers';
-// import {routerReducer} from 'react-router-redux';
+import socketMiddleware from './middleware/socketio-middleware';
 
-// middleware
-// import thunkMiddleware from 'redux-thunk';
-// import createLogger from 'redux-logger';
-// import socketMiddleware from './middleware/socket-middleware';
-
-// const middlewares = [thunkMiddleware, socketMiddleware];
-// const middlewareEnhancer = applyMiddleware(...middlewares);
-const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f);
+const middlewares = [socketMiddleware];
+const middlewareEnhancer = applyMiddleware(...middlewares);
+const enhancers = compose(middlewareEnhancer, window.devToolsExtension ? window.devToolsExtension() : f => f);
 
 const allReducers = combineReducers({
-  ...reducers,
-  // routing: routerReducer
+  ...reducers
 });
 
 const store = createStore(allReducers, enhancers);
 
 export default store;
-
-
